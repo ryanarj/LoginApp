485fbf3cce956d81fc05c80b50257c46bea9e269
Ryan Arjun <karshan813@gmail.com>
2017-01-12 23:01:23 -0500

    Save

---
+++ app/src/main/java/com/example/kr/firstapp/RegisterClass.java
@@ -0,0 +1,9 @@
+package com.example.kr.firstapp;
+
+/**
+ * Created by KR on 1/12/2017.
+ */
+import com.android.volley.toolbox.StringRequest;
+
+public class RegisterClass extends StringRequest{
+}
