3d91745f499241bd8ac64530af0c5f6b8abe28aa
Ryan Arjun <karshan813@gmail.com>
2017-01-15 18:02:47 -0500

    Added the logout functionailty to the app

--- app/src/main/java/com/example/kr/firstapp/MainActivity.java
+++ app/src/main/java/com/example/kr/firstapp/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.kr.firstapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,6 +20,16 @@ public class MainActivity extends AppCompatActivity{
         final EditText etUsername = (EditText) findViewById(R.id.etUsername);
         final EditText etAge = (EditText) findViewById(R.id.etAge);
         final TextView etWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
+        final Button bLogout = (Button) findViewById(R.id.bLogout);
+
+        // If the logout button is clicked on create an intent of the login screen
+        bLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, Login.class);
+                MainActivity.this.startActivity(registerIntent);
+            }
+        });
 
         // Get the Intent retrieve the information from the Database
         Intent intent = getIntent();
